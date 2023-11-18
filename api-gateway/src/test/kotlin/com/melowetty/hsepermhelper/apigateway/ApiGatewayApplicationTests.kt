638dba790650a45bbc592596862262e058c8dd8d
Denis Malinin <melowetty@mail.ru>
2023-11-19 01:58:11 +0500

    add: api-gateway microservice

---
+++ api-gateway/src/test/kotlin/com/melowetty/hsepermhelper/apigateway/ApiGatewayApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.melowetty.hsepermhelper.apigateway
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,5 @@
 rootProject.name = "HsePermHelper"
 
 includeBuild("eureka-server")
+includeBuild("api-gateway")
 includeBuild("users-service")
