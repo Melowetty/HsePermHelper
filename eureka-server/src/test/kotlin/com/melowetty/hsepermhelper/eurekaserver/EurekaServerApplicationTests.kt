7f3bf4eb1deace4140d468632e9acf5a50c79bdb
Denis Malinin <melowetty@mail.ru>
2023-11-19 01:10:31 +0500

    add: eureka-server microservice to project

---
+++ eureka-server/src/test/kotlin/com/melowetty/hsepermhelper/eurekaserver/EurekaServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.melowetty.hsepermhelper.eurekaserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
