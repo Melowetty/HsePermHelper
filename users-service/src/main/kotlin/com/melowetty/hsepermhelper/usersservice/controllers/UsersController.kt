ab041dded7fb51f3715d9deb6ec890a2518f2440
Denis Malinin <melowetty@mail.ru>
2023-11-21 02:37:23 +0500

    add: test endpoints for users service
    * remove: tests from users service
    * add: shared lib dependency

--- users-service/src/main/kotlin/com/melowetty/hsepermhelper/usersservice/controllers/UsersController.kt
+++ users-service/src/main/kotlin/com/melowetty/hsepermhelper/usersservice/controllers/UsersController.kt
@@ -1,12 +1,20 @@
 package com.melowetty.hsepermhelper.usersservice.controllers
 
+import com.melowetty.hsepermhelper.shared.model.Response
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
-@RestController("users")
+@RestController
+@RequestMapping("users")
 class UsersController {
+    @GetMapping("all")
+    fun getAllUsers(): Response<List<String>> {
+        return Response(listOf("123", "1242", "124141"))
+    }
+
     @GetMapping
-    fun getAllUsers(): List<String> {
-        return listOf("123", "1242", "124141")
+    fun getAllAges(): Response<List<Int>> {
+        return Response(listOf(1, 2, 3))
     }
 }
--- users-service/src/test/kotlin/com/melowetty/hsepermhelper/usersservice/UsersServiceApplicationTests.kt
+++ users-service/src/test/kotlin/com/melowetty/hsepermhelper/usersservice/UsersServiceApplicationTests.kt
@@ -3,11 +3,4 @@ package com.melowetty.hsepermhelper.usersservice
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
-class UsersServiceApplicationTests {
 
-	@Test
-	fun contextLoads() {
-	}
-
-}
