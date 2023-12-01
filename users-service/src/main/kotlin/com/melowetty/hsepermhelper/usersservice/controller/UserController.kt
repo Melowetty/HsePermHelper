8e64b16db26b8d5ba3cc8a5665aa745289d48480
Denis Malinin <melowetty@mail.ru>
2023-12-02 04:57:17 +0500

    add: some features for user service
    * add: user not found exception
    * add: users controller for /users
    * add: user controller for /user
    * add: endpoints from old project

--- users-service/src/main/kotlin/com/melowetty/hsepermhelper/usersservice/controller/UserController.kt
+++ users-service/src/main/kotlin/com/melowetty/hsepermhelper/usersservice/controller/UserController.kt
@@ -1,41 +1,110 @@
 package com.melowetty.hsepermhelper.usersservice.controller
 
 import com.melowetty.hsepermhelper.usersservice.dto.UserDto
+import com.melowetty.hsepermhelper.usersservice.exception.UserNotFoundException
 import com.melowetty.hsepermhelper.usersservice.service.UserService
 import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.Parameter
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
+import java.util.*
 
+@RequestMapping("user")
 @RestController
+@Tag(name = "Пользователь", description = "Взаимодействие с пользователем")
 class UserController(
     private val userService: UserService
 ) {
-    @GetMapping("users")
-    fun getUsers(): List<UserDto> {
-        return userService.getUsers()
+    @ResponseStatus(HttpStatus.OK)
+    @Operation(
+        summary = "Получение пользователя по Telegram ID",
+        description = "Позволяет получить пользователя по его Telegram ID"
+    )
+    @GetMapping(
+        produces = [MediaType.APPLICATION_JSON_VALUE]
+    )
+    fun getUserByTelegramId(
+        @Parameter(description = "Telegram ID пользователя")
+        @RequestParam("telegramId")
+        telegramId: Long,
+    ): UserDto {
+        return userService.getUserByTelegramId(telegramId = telegramId) ?: throw UserNotFoundException()
     }
 
+    @ResponseStatus(HttpStatus.OK)
     @Operation(
-        summary = "Регистрация пользователя",
-        description = "Позволяет зарегистрировать пользователя"
+        summary = "Получение пользователя",
+        description = "Позволяет получить пользователя по его ID"
     )
-    @ResponseStatus(HttpStatus.CREATED)
-    @PostMapping(
-        "users",
-        consumes = [MediaType.APPLICATION_JSON_VALUE])
-    fun createUser(
-        @RequestBody userDto: UserDto,
-    ): ResponseEntity<Any> {
-        val result = userService.createUser(userDto)
-        if(userDto.settings.group.isBlank() || userDto.settings.subGroup < 0) {
-            return ResponseEntity
-                .status(HttpStatus.BAD_REQUEST)
-                .contentType(MediaType.APPLICATION_JSON)
-                .body(mapOf(Pair("message", "Неверно указана группа или подгруппа!")))
-        }
-        val status = if (result) HttpStatus.CREATED else HttpStatus.CONFLICT
+    @GetMapping(
+        "{id}",
+        produces = [MediaType.APPLICATION_JSON_VALUE]
+    )
+    fun getUserById(
+        @Parameter(description = "ID пользователя")
+        @PathVariable("id")
+        id: UUID,
+    ): UserDto {
+        return userService.getUserById(id) ?: throw UserNotFoundException()
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @Operation(
+        summary = "Удаление пользователя",
+        description = "Позволяет удалить пользователя по его ID"
+    )
+    @DeleteMapping(
+        "{id}",
+        produces = [MediaType.APPLICATION_JSON_VALUE]
+    )
+    fun deleteUserById(
+        @Parameter(description = "ID пользователя")
+        @PathVariable("id")
+        id: UUID,
+    ) : ResponseEntity<Unit> {
+        val result = userService.deleteUserById(id)
+        val status = if (result) HttpStatus.OK else HttpStatus.INTERNAL_SERVER_ERROR
         return ResponseEntity.status(status).build()
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @Operation(
+        summary = "Удаление пользователя",
+        description = "Позволяет удалить пользователя по его Telegram ID"
+    )
+    @DeleteMapping(
+        produces = [MediaType.APPLICATION_JSON_VALUE]
+    )
+    fun deleteUserByTelegramId(
+        @Parameter(description = "Telegram ID пользователя")
+        @RequestParam("telegramId")
+        telegramId: Long,
+    ): ResponseEntity<Unit> {
+        val result = userService.deleteUserByTelegramId(telegramId)
+        val status = if (result) HttpStatus.OK else HttpStatus.INTERNAL_SERVER_ERROR
+        return ResponseEntity.status(status).build()
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @Operation(
+        summary = "Изменение настроек пользователя",
+        description = "Позволяет изменить настройки пользователя по Telegram ID"
+    )
+    @PatchMapping(
+        consumes = [MediaType.APPLICATION_JSON_VALUE],
+        produces = [MediaType.APPLICATION_JSON_VALUE]
+    )
+    fun updateUserByTelegramId(
+        @Parameter(description = "Telegram ID пользователя")
+        @RequestParam("telegramId")
+        telegramId: Long,
+        @RequestBody
+        @Parameter(description = "Новые настройки пользователя")
+        settings: Map<String, Any>,
+    ): UserDto {
+        return userService.updateUserSettingsByTelegramId(telegramId, settings) ?: throw UserNotFoundException()
     }
 }
---
+++ users-service/src/main/kotlin/com/melowetty/hsepermhelper/usersservice/controller/UsersController.kt
@@ -0,0 +1,68 @@
+package com.melowetty.hsepermhelper.usersservice.controller
+
+import com.melowetty.hsepermhelper.usersservice.dto.UserDto
+import com.melowetty.hsepermhelper.usersservice.exception.UserNotFoundException
+import com.melowetty.hsepermhelper.usersservice.service.UserService
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+
+@Tag(name = "Пользователи", description = "Взаимодействие с пользователями")
+@RestController
+@RequestMapping("users")
+class UsersController(
+    private val userService: UserService
+) {
+    @Operation(
+        summary = "Список пользователей",
+        description = "Позволяет получить список всех пользователей"
+    )
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping(
+        produces = [MediaType.APPLICATION_JSON_VALUE]
+    )
+    // TODO: Необходимо добавить пагинацию
+    fun getUsers(): List<UserDto> {
+        return userService.getUsers()
+    }
+
+    @Operation(
+        summary = "Регистрация пользователя",
+        description = "Позволяет зарегистрировать пользователя"
+    )
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping(
+        consumes = [MediaType.APPLICATION_JSON_VALUE])
+    fun createUser(
+        @RequestBody userDto: UserDto,
+    ): ResponseEntity<Any> {
+        val result = userService.createUser(userDto)
+        if(userDto.settings.group.isBlank() || userDto.settings.subGroup < 0) {
+            return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(mapOf(Pair("message", "Неверно указана группа или подгруппа!")))
+        }
+        val status = if (result) HttpStatus.CREATED else HttpStatus.CONFLICT
+        return ResponseEntity.status(status).build()
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @Operation(
+        summary = "Обновление пользователя",
+        description = "Позволяет обновить данные пользователя"
+    )
+    @PutMapping(
+        "users",
+        consumes = [MediaType.APPLICATION_JSON_VALUE],
+        produces = [MediaType.APPLICATION_JSON_VALUE]
+    )
+    fun updateUser(
+        @RequestBody userDto: UserDto,
+    ): UserDto {
+        return userService.updateUser(userDto) ?: throw UserNotFoundException()
+    }
+}
