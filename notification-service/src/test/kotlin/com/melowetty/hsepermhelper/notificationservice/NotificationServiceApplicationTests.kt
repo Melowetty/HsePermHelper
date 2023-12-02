a4af0cb15aad1897926e5339a02000f6f110b1a6
Denis Malinin <melowetty@mail.ru>
2023-12-03 01:39:36 +0500

    add: notification service

---
+++ notification-service/src/test/kotlin/com/melowetty/hsepermhelper/notificationservice/NotificationServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.melowetty.hsepermhelper.notificationservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
