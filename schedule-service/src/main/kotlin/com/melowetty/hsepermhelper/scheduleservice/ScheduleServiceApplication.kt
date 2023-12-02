d2161d9bb11b4878fcbb98a66e524d7e78ef92f2
Denis Malinin <melowetty@mail.ru>
2023-12-03 01:32:08 +0500

    add: schedule service

---
+++ schedule-service/src/main/kotlin/com/melowetty/hsepermhelper/scheduleservice/ScheduleServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.melowetty.hsepermhelper.scheduleservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ScheduleServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<ScheduleServiceApplication>(*args)
+}
