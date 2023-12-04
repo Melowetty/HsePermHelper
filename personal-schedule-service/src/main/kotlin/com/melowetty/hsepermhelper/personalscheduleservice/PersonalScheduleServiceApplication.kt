e28c6a7ae8600c6e6ba2d86c527b55b43ec37104
Denis Malinin <melowetty@mail.ru>
2023-12-05 04:12:50 +0500

    add: personal schedule service

---
+++ personal-schedule-service/src/main/kotlin/com/melowetty/hsepermhelper/personalscheduleservice/PersonalScheduleServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.melowetty.hsepermhelper.personalscheduleservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PersonalScheduleServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<PersonalScheduleServiceApplication>(*args)
+}
---
+++ personal-schedule-service/src/test/kotlin/com/melowetty/hsepermhelper/personalscheduleservice/PersonalScheduleServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.melowetty.hsepermhelper.personalscheduleservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PersonalScheduleServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
