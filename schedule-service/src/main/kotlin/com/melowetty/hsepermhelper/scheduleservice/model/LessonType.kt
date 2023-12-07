e70a1a2fd8bd3a54c254b341f44b3dec405d2941
Denis Malinin <melowetty@mail.ru>
2023-12-07 05:23:03 +0500

    remove: unused field in lesson type

--- schedule-service/src/main/kotlin/com/melowetty/hsepermhelper/scheduleservice/model/LessonType.kt
+++ schedule-service/src/main/kotlin/com/melowetty/hsepermhelper/scheduleservice/model/LessonType.kt
@@ -1,12 +1,36 @@
 package com.melowetty.hsepermhelper.scheduleservice.model
 
 enum class LessonType(val type: String) {
-    LECTURE("Лекция"),
-    SEMINAR("Семинар"),
-    EXAM("Экзамен"),
+    LECTURE("Лекция") {
+        override fun reformatSubject(subject: String): String {
+            return subject
+                .replace("(лекция)", "")
+                .replace("(лекции)", "")
+                .trim()
+        }
+    },
+    SEMINAR("Семинар") {
+        override fun reformatSubject(subject: String): String {
+            return subject.replace("(семинар)", "").trim()
+        }
+    },
+    EXAM("Экзамен") {
+        override fun reformatSubject(subject: String): String {
+            return subject.replace("ЭКЗАМЕН", "").trim()
+        }
+    },
     INDEPENDENT_EXAM("Независимый экзамен"),
-    TEST("Зачёт"),
-    PRACTICE("Практика"),
+    TEST("Зачёт") {
+        override fun reformatSubject(subject: String): String {
+            return subject.replace("ЗАЧЕТ", "").trim()
+        }
+    },
+    PRACTICE("Практика") {
+        override fun reformatSubject(subject: String): String {
+            return if (subject == "ПРАКТИКА") ""
+            else subject
+        }
+    },
     COMMON_MINOR("Майнор"),
     MINOR("Майнор"),
     COMMON_ENGLISH("Английский"),
@@ -14,6 +38,16 @@ enum class LessonType(val type: String) {
     STATEMENT("Ведомость"),
     ICC("МКД"),
     UNDEFINED_AED("ДОЦ по выбору"),
-    AED("ДОЦ"),
+    AED("ДОЦ") {
+        override fun reformatSubject(subject: String): String {
+            return subject
+                .replace("[", "")
+                .replace("]", "")
+                .trim()
+        }
+    },
     EVENT("Мероприятие");
+    open fun reformatSubject(subject: String): String {
+        return subject
+    }
 }
