3635c48bea52cc0c881eb60d6ea92b93c4501ead
Denis Malinin <melowetty@mail.ru>
2024-01-11 00:55:43 +0500

    remove: application tests from schedule service

--- schedule-service/src/test/kotlin/com/melowetty/hsepermhelper/scheduleservice/ScheduleServiceApplicationTests.kt
+++ schedule-service/src/test/kotlin/com/melowetty/hsepermhelper/scheduleservice/ScheduleServiceApplicationTests.kt
@@ -3,11 +3,4 @@ package com.melowetty.hsepermhelper.scheduleservice
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
-class ScheduleServiceApplicationTests {
 
-    @Test
-    fun contextLoads() {
-    }
-
-}
