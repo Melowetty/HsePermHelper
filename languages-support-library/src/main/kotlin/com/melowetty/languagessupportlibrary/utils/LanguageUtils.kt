d52ec83fcf2e7f2cf7a566ee3abad84d6ad9a758
Denis Malinin <melowetty@mail.ru>
2024-01-10 18:35:10 +0500

    fix: get language from header when header is lowercase

--- languages-support-library/src/main/kotlin/com/melowetty/languagessupportlibrary/utils/LanguageUtils.kt
+++ languages-support-library/src/main/kotlin/com/melowetty/languagessupportlibrary/utils/LanguageUtils.kt
@@ -6,7 +6,8 @@ class LanguageUtils {
     companion object {
         private const val LANGUAGE_HEADER = "X-Language"
         fun getLanguageFromHeaders(headers: Map<String, String>): Language {
-            val headerValue = headers.get(LANGUAGE_HEADER)
+            val mappedHeaders = headers.mapKeys { it.key.lowercase() }
+            val headerValue = mappedHeaders.get(LANGUAGE_HEADER.lowercase())
             return if(headerValue != null) {
                 Language.fromString(headerValue) ?: Language.RUSSIAN
             } else Language.RUSSIAN
